818f266e36b1fb8a5d49e305d3385dceeb5203f0
HarryNKM <harrynakum05@gmail.com>
2024-09-23 18:13:34 +0530

    new

--- lib/screen/splash/splash_screen.dart
+++ lib/screen/splash/splash_screen.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,22 +9,41 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  void initState() {
+  /*void initState() {
     super.initState();
     Future.delayed(Duration(seconds: 3,),() {
       Navigator.pushReplacementNamed(context, 'home');
     },);
-  }
+  }*/
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
           Image.network(
-            "https://i.pinimg.com/736x/15/c2/f0/15c2f0641ffd584bf859649c6120ce33.jpg",
+            "https://wallpapers.com/images/hd/planet-earth-home-screen-snbr0jc9ifx224vk.jpg",
             fit: BoxFit.cover,
             width: MediaQuery.sizeOf(context).width,
+            height: MediaQuery.sizeOf(context).height,
           ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Know Your Planets",
+                  style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+                Text(
+                  "Explore Our Solar system in real time and know about the planets.",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
